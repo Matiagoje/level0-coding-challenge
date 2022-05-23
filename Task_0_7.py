7e13368ff92d2250dad2ad1eb3610dac2c8d2081
Matiagoje <frangelinematiagoje@gmail.com>
2022-05-23 10:44:19 +0200

    modified

--- Task_0_7.py
+++ Task_0_7.py
@@ -1,13 +1,14 @@
 def fahrenheit_to_celsius(fahrenheit_input):
     celsius = (fahrenheit_input - 32)* (5/9)
     celsius = int(celsius)
-    print("celsius = ", celsius)
+    return celsius
 
 fahrenheit_to_celsius(86)
 
 def celsius_to_fahrenheit(celsius_input):
     fahrenheit = (celsius_input * 1.8) + 32
     fahrenheit = int(fahrenheit)
-    print("fahrenheit = ", fahrenheit)
+    return fahrenheit
 
-celsius_to_fahrenheit(35)
+print("Celsius = ", fahrenheit_to_celsius(86))
+print("Fahrenheit = ", celsius_to_fahrenheit(35))
